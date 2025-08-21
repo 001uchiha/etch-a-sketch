c1d737884c24a40ceda18fb9b5c0411074b001b1
001uchiha <mohaking321@gmail.com>
2025-08-21 22:43:09 +0200

    feat: add hover affect to grid cells

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const container = document.querySelector('.grid-container');
 const gridSize = 16;
 
+let isMouseDown = false;
+
+document.addEventListener('mousedown', () => isMouseDown=true);
+document.addEventListener('mouseup', () => isMouseDown=false);
+
+function paintCell(cell) {
+	cell.style.backgroundColor = 'blue';
+}
+
 function createGrid(size) {
 	container.innerHTML = '';
 
 	for (let i = 0; i < gridSize * gridSize; i++) {
 		const cell = document.createElement('div');
-		container.appendChild(cell).className = 'cell';
+		cell.classList.add('cell');
+
+		cell.addEventListener('click', () => paintCell(cell));
+		cell.addEventListener('mouseover', () => {
+			if (isMouseDown) paintCell(cell)
+		});
+		container.appendChild(cell);
 	}
 }
 
