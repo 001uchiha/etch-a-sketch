a273b09d1f6ff7c4f5821c0944af0070303ddc20
001uchiha <mohaking321@gmail.com>
2025-09-21 08:38:30 +0200

    feat: add image icon and color palette

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const container = document.querySelector('.grid-container');
 const generateBtn = document.getElementById('generateBtn');
+const modeBtn = document.getElementById('modeBtn');
+const iconBox = document.getElementById('iconBox');
+const palette = document.getElementById('palette');
+const colorBar = document.getElementById('colorBar');
 const gridSize = 16;
 
+let eraserMode = false;
 let isMouseDown = false;
+let drawColor = '#75FBFD';
+
+iconBox.addEventListener('click', () => {
+	palette.style.display = palette.style.display === 'grid' ? 'none' : 'grid';
+});
+
 
 document.addEventListener('mousedown', () => isMouseDown=true);
 document.addEventListener('mouseup', () => isMouseDown=false);
 
+palette.querySelectorAll('div').forEach(div => {
+	div.addEventListener('click', () => {
+		drawColor = div.style.background;
+		colorBar.style.background = drawColor;
+	});
+});
+
 function paintCell(cell) {
-	cell.style.backgroundColor = 'blue';
+	if (eraserMode) {
+		cell.style.backgroundColor = 'lightgray';
+	} else {
+		cell.style.backgroundColor = drawColor;
+	}
 }
 
 function createGrid(size) {
@@ -33,5 +55,10 @@ generateBtn.addEventListener('click', () => {
 	if (size > 100) size = 100;
 	createGrid(size);
 });
+
+modeBtn.addEventListener('click', () => {
+	eraserMode = !eraserMode;
+	modeBtn.textContent = eraserMode ? 'Mode: Erase' : 'Mode: Draw';
+})
 
 createGrid(gridSize);
